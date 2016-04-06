31e3fa168c5f70b75150872ee55876ad019457a4
Teddy Ort <teddy@mit.edu>
2016-04-05 23:09:51 -0400

    Refactos the controller node as a class

--- src/controller/scripts/controller_node.py
+++ src/controller/scripts/controller_node.py
@@ -6,15 +6,8 @@ from dynamixel_msgs.msg import JointState
 from robot_driver import RobotDriver
 
 # Default globals
-state = "Init"
-temp = 0
-holder = True
-rng = 100
-joint2_load = 0
-joint2_avg = 0
 long_term_alpha = 0.1
 short_term_alpha = 0.5
-
 grip_close = 3.0
 grip_open = 1.8
 wrist_rotate = 3.14
@@ -27,109 +20,130 @@ solder_dispense_time = 5
 load_threshold = 0.006
 noload_threshold = 0.001
 
-def temp_callback(msg):
-    global temp
-    temp = msg.data
+class ControllerNode(object):
+    def __init__(self):
+        self.node_name = "controller_node"
+        # Setup Params
+        self.L0     = self.setupParam("~L0",0.22) # in meters
+        self.L1     = self.setupParam("~L1",0.22) # in meters
+        self.L2     = self.setupParam("~L2",0.22) # in meters
+        
+        # Init self.state variables
+        self.state = "Init"
+        self.temp = 0
+        self.holder = True
+        self.rng = 100
+        self.joint2_load = 0
+        self.joint2_avg = 0
 
-def holder_callback(msg):
-    global holder
-    holder = msg.data
+        #Create publishers and subscribers
+        self.pub_wrist = rospy.Publisher('~wrist_cmd', Float32, queue_size=10)
+        self.pub_grip = rospy.Publisher('~grip_cmd', Float32, queue_size=10)
+        self.pub_feeder = rospy.Publisher('~feeder_cmd', Bool, queue_size=10)
+        self.sub_temp = rospy.Subscriber("~solder/temp", Float32, self.temp_callback)
+        self.sub_holder = rospy.Subscriber("~solder/holder", Bool, self.holder_callback)
+        self.sub_range = rospy.Subscriber("~range", Float32, self.range_callback)
+        self.sub_load = rospy.Subscriber("/joint2_controller/self.state", JointState, self.load_callback)
+        rospy.loginfo("[%s] has started.", self.node_name)
+        self.controller()
+        
+    def temp_callback(self, msg):
+        self.temp = msg.data
 
-def range_callback(msg):
-    global rng
-    rng = msg.data
+    def holder_callback(self, msg):
+        self.holder = msg.data
 
-def load_callback(msg):
-    global joint2_load, joint2_avg
-    joint2_load += short_term_alpha * (msg.load - joint2_load)
-    joint2_avg += long_term_alpha * (msg.load - joint2_avg)
+    def range_callback(self, msg):
+        self.rng = msg.data
 
-def controller():
-    global state
-    rospy.init_node('controller', anonymous=False)
+    def load_callback(self, msg):
+        self.joint2_load += short_term_alpha * (msg.load - self.joint2_load)
+        self.joint2_avg += long_term_alpha * (msg.load - self.joint2_avg)
 
-    #Create publishers and subscribers
-    pub_wrist = rospy.Publisher('/icarus/wrist_cmd', Float32, queue_size=10)
-    pub_grip = rospy.Publisher('/icarus/grip_cmd', Float32, queue_size=10)
-    pub_feeder = rospy.Publisher('/icarus/feeder_cmd', Bool, queue_size=10)
-    sub_temp = rospy.Subscriber("/icarus/solder/temp", Float32, temp_callback)
-    sub_holder = rospy.Subscriber("/icarus/solder/holder", Bool, holder_callback)
-    sub_range = rospy.Subscriber("/icarus/range", Float32, range_callback)
-    sub_load = rospy.Subscriber("/joint2_controller/state", JointState, load_callback)
+    def controller(self):
 
-    #Create the arm class for controlling dynamixels
-    arm = RobotDriver.Driver('arm', 0.22, 0.22, 0.22)
+        #Create the arm class for controlling dynamixels
+        arm = RobotDriver.Driver('arm', self.L0, self.L1, self.L2)
 
-    rate = rospy.Rate(20)
-    while not rospy.is_shutdown():
-        if state == "Init":
-            rospy.loginfo("Controller Initialized")
-            state = "Start"
-        elif state == "Start":
-            rospy.loginfo("State: Start")
-            arm.move_joint([0.0,0.5, -2.0],2)
-            #arm.move_cartesian([0.20, 0.0, 0.11], 2)
-            pub_grip.publish(grip_open)
-            pub_wrist.publish(0)
-            rospy.sleep(1)
-            state = "Wait for Range"
-            rospy.loginfo("State: Wait for Range")
-        elif state == "Wait for Range":
-            if rng < range_threshold:
-                rospy.sleep(0.75)
-                pub_grip.publish(grip_close)
-                state = "Gripped"
-                rospy.loginfo("State: Gripped")
-        elif state == "Gripped":
-            if not holder:
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown():
+            if self.state == "Init":
+                rospy.loginfo("Controller Initialized")
+                self.state = "Start"
+            elif self.state == "Start":
+                rospy.loginfo("self.State: Start")
+                arm.move_joint([0.0,0.5, -2.0],2)
+                #arm.move_cartesian([0.20, 0.0, 0.11], 2)
+                self.pub_grip.publish(grip_open)
+                self.pub_wrist.publish(0)
                 rospy.sleep(1)
-                pub_wrist.publish(wrist_rotate)
-                state = "Rotated"
-                rospy.loginfo("State: Rotated")            
-        elif state == "Rotated":
-            if not temp > min_solder_temp:
-                rospy.logwarn("Solder too cold. Please turn on to proceed.")
-            else:
-                rospy.sleep(tin_wait_time)
-                pub_feeder.publish(True)
-                rospy.sleep(solder_tin_time)
-                pub_feeder.publish(False)
-                state = "Tinned"
-                rospy.loginfo("State: Tinned, waiting for load")
-        elif state == "Tinned":
-            #print "Delta", joint2_avg - joint2_load
-            if joint2_avg - joint2_load > load_threshold:
-                rospy.loginfo("Dispensing...")
-                rospy.sleep(dispense_wait_time)
-                pub_feeder.publish(True)
-                rospy.sleep(solder_dispense_time)
-                pub_feeder.publish(False)
-                state = "Dispensed"
-                rospy.loginfo("State: Dispensed")
-            elif holder:
-                state = "Retract"
-                rospy.loginfo("State: Retract")
+                self.state = "Wait for Range"
+                rospy.loginfo("self.State: Wait for Range")
+            elif self.state == "Wait for Range":
+                if self.rng < range_threshold:
+                    rospy.sleep(0.75)
+                    self.pub_grip.publish(grip_close)
+                    self.state = "Gripped"
+                    rospy.loginfo("self.State: Gripped")
+            elif self.state == "Gripped":
+                if not self.holder:
+                    rospy.sleep(1)
+                    self.pub_wrist.publish(wrist_rotate)
+                    self.state = "Rotated"
+                    rospy.loginfo("self.State: Rotated")
+            elif self.state == "Rotated":
+                if not self.temp > min_solder_temp:
+                    rospy.logwarn("Solder too cold. Please turn on to proceed.")
+                else:
+                    rospy.sleep(tin_wait_time)
+                    self.pub_feeder.publish(True)
+                    rospy.sleep(solder_tin_time)
+                    self.pub_feeder.publish(False)
+                    self.state = "Tinned"
+                    rospy.loginfo("self.State: Tinned, waiting for load")
+            elif self.state == "Tinned":
+                #print "Delta", joint2_avg - joint2_load
+                if self.joint2_avg - self.joint2_load > load_threshold:
+                    rospy.loginfo("Dispensing...")
+                    rospy.sleep(dispense_wait_time)
+                    self.pub_feeder.publish(True)
+                    rospy.sleep(solder_dispense_time)
+                    self.pub_feeder.publish(False)
+                    self.state = "Dispensed"
+                    rospy.loginfo("self.State: Dispensed")
+                elif self.holder:
+                    self.state = "Retract"
+                    rospy.loginfo("self.State: Retract")
 
-        elif state == "Dispensed":
-            #print "Delta", joint2_avg - joint2_load
-            if joint2_avg - joint2_load < noload_threshold:
-                state = "Tinned"
-                rospy.loginfo("State: Tinned")
-            elif holder:
-                state = "Retract"
-                rospy.loginfo("State: Retract")
+            elif self.state == "Dispensed":
+                #print "Delta", joint2_avg - joint2_load
+                if self.joint2_avg - self.joint2_load < noload_threshold:
+                    self.state = "Tinned"
+                    rospy.loginfo("self.State: Tinned")
+                elif self.holder:
+                    self.state = "Retract"
+                    rospy.loginfo("self.State: Retract")
 
-        elif state == "Retract":
-            arm.move_joint([0.0,0.25, -2.25],2)
-            pub_grip.publish(grip_open)
-            rospy.sleep(0.5)
-            arm.move_joint([0.0, 0.55, -2.5],2)
-            state = "Start"
+            elif self.state == "Retract":
+                arm.move_joint([0.0,0.25, -2.25],2)
+                self.pub_grip.publish(grip_open)
+                rospy.sleep(0.5)
+                arm.move_joint([0.0, 0.55, -2.5],2)
+                self.state = "Start"
+            rate.sleep()
 
-        rate.sleep()
+    def setupParam(self,param_name,default_value):
+        value = rospy.get_param(param_name,default_value)
+        rospy.set_param(param_name,value) #Write to parameter server for transparancy
+        rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
+        return value
+
+    def onShutdown(self):
+        # This should call the relax all service of the robot_driver package once that's available
+        rospy.loginfo("[%s] shutting down.", self.node_name)
 
 if __name__ == '__main__':
-    try:
-        controller()
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node('controller_node',anonymous=False)
+    controller_node = ControllerNode()
+    rospy.on_shutdown(controller_node.onShutdown)
+    rospy.spin()
