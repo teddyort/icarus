7a375794ede18aaaa2c3bc4218efdb6a7478a4dc
Teddy Ort <teddy@mit.edu>
2016-04-12 20:15:46 -0400

    Creates a robot_driver_node for interfacing with the dynamixels

---
+++ src/robot_driver/scripts/robot_driver_node.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+import rospy
+from robot_driver import Kinematics
+from robot_driver.srv import MoveCartesian, MoveJoint
+from sensor_msgs.msg import JointState
+import tf
+import actionlib
+from trajectory_msgs.msg import JointTrajectoryPoint
+from control_msgs.msg import JointTrajectoryAction, JointTrajectoryGoal, FollowJointTrajectoryAction, FollowJointTrajectoryGoal
+import numpy as np
+
+# Robot Driver Node
+# Authors: Teddy Ort
+# Inputs: 
+# Outputs: 
+
+class RobotDriverNode(object):
+    def __init__(self):
+
+        # Retrieve Parameters
+        self.node_name = 'robot_driver_node'
+        links = self.setupParameter("~links", {'L0': 0.245, 'L1': 0.22, 'L2': 0.24})
+        self.workspace = self.setupParameter("~workspace", {'x_lim': 0, 'y_lim': 0, 'z_lim': 0})
+        self.max_vel = self.setupParameter("~max_vel", 0.5)
+
+        # Setup the subscriber
+        self.sub_joints = rospy.Subscriber("~joint_states", JointState, self.jointsCallback)
+        self.msg_joint_state = JointState()
+
+        # Setup the action server
+        self.name = 'arm'
+        self.kinematics = Kinematics.Kinematics(links['L0'], links['L1'],links['L2'])
+        self.jta = actionlib.SimpleActionClient('/arm_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
+        rospy.loginfo('Waiting for joint trajectory action')
+        self.jta.wait_for_server()
+        rospy.loginfo('Found joint trajectory action!')
+
+        # Start the service servers
+        self.srv = rospy.Service("~move_cartesian", MoveCartesian, self.handle_move_cartesian)
+        self.srv = rospy.Service("~move_joint", MoveJoint, self.handle_move_joint)
+
+        rospy.loginfo("[%s] has started", self.node_name)
+
+    def jointsCallback(self, msg_joint_state):
+        self.msg_joint_state = msg_joint_state
+
+    def handle_move_joint(self, msg_joints):
+        joints = (msg_joints.joints.vector.x, msg_joints.joints.vector.y, msg_joints.joints.vector.z)
+        self.move_joint(joints, self.msg_joint_state.position, self.max_vel)
+        return True
+
+    def handle_move_cartesian(self, msg_cart):
+        point = msg_cart.point
+        point.header.frame_id = msg_cart.header.frame_id if not point.header.frame_id == "" else "base_link"
+        tros = tf.TransformerROS()
+        dest = tros.transformPoint("base_link", point)
+        self.move_cartesian((dest.point.x, dest.point.y, dest.point.z), self.msg_joint_state.position, self.max_vel)
+        return True
+
+    def move_joint(self, dest, state, max_v):
+        # Calculate duration
+        t = max(abs(np.array(dest) - np.array(state)))/max_v
+
+        # Create the trajectory
+        goal = FollowJointTrajectoryGoal()
+        goal.trajectory.joint_names = ['joint1', 'joint2', 'joint3']
+        point = JointTrajectoryPoint()
+        point.positions = dest
+        point.time_from_start = rospy.Duration(t)
+        goal.trajectory.points.append(point)
+        self.jta.send_goal_and_wait(goal)
+
+    def move_cartesian(self, pose, state, max_v):
+        angles = self.kinematics.ikin(pose)
+        self.move_joint(angles, state, max_v)
+        rospy.loginfo("Moving to angles: %s", angles)
+
+    def setupParameter(self, param_name, default_value):
+        value = rospy.get_param(param_name, default_value)
+        rospy.set_param(param_name, value)  # Write to parameter server for transparancy
+        rospy.loginfo("[%s] %s = %s " % (self.node_name, param_name, value))
+        return value
+
+if __name__ == '__main__':
+    rospy.init_node('robot_driver_node', anonymous=False)
+    robot_driver_node = RobotDriverNode()
+    rospy.spin()
